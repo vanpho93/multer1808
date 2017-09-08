90cc9cad106c675080e457d6889be1d95d11830f
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-08 21:12:43 +0700

    aaa

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('home'));
 
-app.post('/signup', upload.single('profile'), (req, res) => {
-    res.send(req.body);
+const filesConfig = [{ name: 'chinh', maxCount: 1 }, { name: 'phu', maxCount: 2 }];
+
+app.post('/signup', upload.fields(filesConfig), (req, res) => {
+    res.send(req.files);
 });
+
+app.use((err, req, res, next) => {// eslint-disable-line
+    res.send(err.message);
+});
+
+app.get('*', (req, res) => res.send('Cannot find this link'));
 
 reload(app);
 
